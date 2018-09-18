52219af8ea928244086b190fe4deba49fa810f05
emaster1 <emilia.a.sterna@gmail.com>
2018-09-18 23:21:06 +0200

    Zmiany

--- Zmienne.java
+++ Zmienne.java
@@ -4,6 +4,6 @@ int liczba; //Deklaracja
 liczba = 5; //Inicjalizacja
 int liczba2 = 10;
 System.out.println("liczba" + " " + liczba);
-System.out.println("liczba2" + " " + liczba2);
+//System.out.println("liczba2" + " " + liczba2);
 }
 }
