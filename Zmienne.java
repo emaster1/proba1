ba54fcd725cadf1c2bfc02d747c8b36b000bca73
emaster1 <emilia.a.sterna@gmail.com>
2018-09-18 23:16:18 +0200

    added Zmienne.java

---
+++ Zmienne.java
@@ -0,0 +1,9 @@
+public class Zmienne{
+public static void main (String[] args){
+int liczba; //Deklaracja
+liczba = 5; //Inicjalizacja
+int liczba2 = 10;
+System.out.println("liczba" + " " + liczba);
+System.out.println("liczba2" + " " + liczba2);
+}
+}
